33a2fe81d365a72a038f74d650d2dfcf644900d9
faizan1487 <faizanahmed14887@gmail.com>
2024-07-05 12:48:25 +0500

    gitignorecreated, bugs resolved in docker-compose, db connected with backend service

--- app/settings.py
+++ app/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ambassador',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',  # This should be the service name defined in Docker Compose
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
