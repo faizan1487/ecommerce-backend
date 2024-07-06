24d4769e9a94ab67237104db955a1139c0a65023
faizan1487 <faizanahmed14887@gmail.com>
2024-07-06 10:00:37 +0500

    usermanager created, model register in admin.py

--- core/models.py
+++ core/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not password:
+            raise ValueError('User must have a password')
+        
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = False
+        user.is_staff = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not password:
+            raise ValueError('User must have a password')
+        
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
@@ -13,3 +44,5 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS=[]
+    
+    objects = UserManager()
