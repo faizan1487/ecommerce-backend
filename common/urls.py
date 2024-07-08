ef1794edf63ae3b3f918504bce9d47dd8c5c8804
faizan1487 <faizanahmed14887@gmail.com>
2024-07-08 10:12:35 +0500

    login api created

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import RegisterAPIView
+from .views import RegisterAPIView, LoginAPIView
 
 app_name = 'common'
 
 urlpatterns = [
-    path('register', RegisterAPIView.as_view(), name='register')
+    path('register', RegisterAPIView.as_view(), name='register'),
+    path('login', LoginAPIView.as_view(), name='login')
 ]
--- common/views.py
+++ common/views.py
@@ -4,7 +4,7 @@ from rest_framework import exceptions
 from rest_framework import status
 
 from .serializers import UserSerializer
-
+from core.models import User
 
 # Create your views here.
 class RegisterAPIView(APIView):
@@ -21,3 +21,20 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        
+        user = User.objects.filter(email=email).first()
+        
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+        
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect password!')
+        
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
