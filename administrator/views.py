03afc73527a410fbf1fd701a4bdaae43d27c14b6
faizan1487 <faizanahmed14887@gmail.com>
2024-07-11 12:40:25 +0500

    ambassador api created

--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+from core.models import User
+from common.serializers import UserSerializer
+
+class AmbassadorAPIView(APIView):
+    def get(self, _):
+        ambassadors = User.objects.filter(is_ambassador=True)
+        serializer = UserSerializer(ambassadors, many=True)
+        return Response(serializer.data)
